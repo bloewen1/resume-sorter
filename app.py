d1349fdd11ae943d91786400af81bedd12e51634
aaronlaitner <aslaitner@duck.com>
2023-08-03 15:53:54 -0600

    Added comments

--- app.py
+++ app.py
@@ -13,15 +13,19 @@ def index():
 
 @app.route('/parse', methods=['POST'])
 def parse_files():
+    # Check if 'file' is present in the request
     if 'file' not in request.files:
         return jsonify({"error": "No file part"})
 
+    # Get the uploaded zip file
     zip_file = request.files['file']
 
+    # Check if the uploaded file is a valid zip file
     if not zipfile.is_zipfile(zip_file):
         return jsonify({"error": "Invalid zip file"})
 
     try:
+        # Extract the contents of the zip file
         file_contents = {}
         with zipfile.ZipFile(zip_file, 'r') as zip_archive:
             for filename in zip_archive.namelist():
@@ -30,11 +34,14 @@ def parse_files():
     except zipfile.BadZipFile:
         return jsonify({"error": "Bad Zip File"})
 
+    # Get the list of search words from the form
     search_words = request.form.getlist('search_word')
 
+    # Check if search words are provided
     if not search_words:
         return jsonify({"error": "No search words provided"})
 
+    # Process each file in the zip and calculate scores
     results = {}
     counter = 0
     for filename, file_content in file_contents.items():
@@ -52,10 +59,12 @@ def parse_files():
 
         results[filename] = {"score": score}
     
+    # Sort the results based on the score in descending order
     sorted_results = dict(sorted(results.items(), key=lambda item: item[1]['score'], reverse=True))
     return jsonify(sorted_results)
 
 def parse_word_document(file_content):
+    # Parse the content of a Word document
     doc = Document(BytesIO(file_content))
     content = ""
     for paragraph in doc.paragraphs:
@@ -63,6 +72,7 @@ def parse_word_document(file_content):
     return content
 
 def parse_pdf(file_content):
+    # Parse the content of a PDF document
     content = ""
     try:
         pdf_file = PyPDF2.PdfReader(BytesIO(file_content))
@@ -74,6 +84,7 @@ def parse_pdf(file_content):
     return content
 
 if __name__ == '__main__':
-    # Only need 250 mb of storage
+    # Set the maximum content length for file uploads to 250 MB
     app.config['MAX_CONTENT_LENGTH'] = 250 * 1024 * 1024
     app.run(debug=True)
+
